7e2c4e24e2566ded8d36555a3c4e6e5509bdb1cd
Mohamed Al-kassas <elasasmohamed97@gmail.com>
2023-10-26 01:04:11 +0300

    hide add/close btn from about page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({title, onAdd, showAdd}) => {
-  
+  const location = useLocation();  
 
   return (
     <header className="header">
       <h1>{title} </h1>
-      < Button 
+      {location.pathname === "/" && < Button 
         text={showAdd ? "Close" : "Add"} 
         color={showAdd ? "red" : "green"} 
         onAdd={onAdd}
-        />
+        />}
     </header>
   )
 }
